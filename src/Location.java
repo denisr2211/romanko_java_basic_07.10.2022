64c95714da9b8b1aec7deda577c2cfb0853ddfea
Den <denis.r2211@gmail.com>
2022-10-07 17:49:13 +0300

    second commit

---
+++ src/Location.java
@@ -0,0 +1,8 @@
+public class Location {
+    public static void main(String[] args) {
+        double latitude = 48.4659984;
+        double longitude = 35.0552501;
+        String myLocation = "My location:" + "\n" +  "широта - " + latitude + "\n" + "долгота - " + longitude;
+        System.out.println(myLocation);
+    }
+}
